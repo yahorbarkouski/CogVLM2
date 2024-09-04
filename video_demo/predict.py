58d4f8ba517bd5d44c064daeb87cff3b878a4912
Yahor Barkouski <yahor.barkouski@gmail.com>
2024-09-04 12:22:59 +0200

    chore: load video fixes

--- video_demo/predict.py
+++ video_demo/predict.py
@@ -55,10 +55,10 @@ class Predictor(BasePredictor):
             template_version=strategy
         )
         inputs = {
-            'input_ids': inputs['input_ids'].unsqueeze(0).to('cuda'),
-            'token_type_ids': inputs['token_type_ids'].unsqueeze(0).to('cuda'),
-            'attention_mask': inputs['attention_mask'].unsqueeze(0).to('cuda'),
-            'images': [[inputs['images'][0].to('cuda').to(TORCH_TYPE)]],
+            'input_ids': inputs['input_ids'].unsqueeze(0).to(DEVICE),
+            'token_type_ids': inputs['token_type_ids'].unsqueeze(0).to(DEVICE),
+            'attention_mask': inputs['attention_mask'].unsqueeze(0).to(DEVICE),
+            'images': [[inputs['images'][0].to(DEVICE).to(TORCH_TYPE)]],
         }
         gen_kwargs = {
             "max_new_tokens": 2048,
@@ -72,22 +72,22 @@ class Predictor(BasePredictor):
             outputs = self.model.generate(**inputs, **gen_kwargs)
             outputs = outputs[:, inputs['input_ids'].shape[1]:]
             response = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-            return response
+        return response
 
-    def load_video(video_data, strategy='chat'):
+    @staticmethod
+    def load_video(video: Path, strategy='chat') -> Any:
         bridge.set_bridge('torch')
-        mp4_stream = video_data
+        with open(video, 'rb') as f:
+            mp4_stream = f.read()
         num_frames = 24
         decord_vr = VideoReader(io.BytesIO(mp4_stream), ctx=cpu(0))
-
         frame_id_list = None
         total_frames = len(decord_vr)
         if strategy == 'base':
             clip_end_sec = 60
             clip_start_sec = 0
             start_frame = int(clip_start_sec * decord_vr.get_avg_fps())
-            end_frame = min(total_frames,
-                            int(clip_end_sec * decord_vr.get_avg_fps())) if clip_end_sec is not None else total_frames
+            end_frame = min(total_frames, int(clip_end_sec * decord_vr.get_avg_fps())) if clip_end_sec is not None else total_frames
             frame_id_list = np.linspace(start_frame, end_frame - 1, num_frames, dtype=int)
         elif strategy == 'chat':
             timestamps = decord_vr.get_frame_timestamp(np.arange(total_frames))

8dfe2bedffe04cae20baed3964dae9d275c74f5d
Yahor Barkouski <yahor.barkouski@gmail.com>
2024-09-04 14:40:00 +0200

    chore: load video fixes

--- video_demo/predict.py
+++ video_demo/predict.py
@@ -18,10 +18,8 @@ class Predictor(BasePredictor):
     def setup(self) -> None:
         """Load the model into memory to make running multiple predictions efficient"""
         local_files_only = False  # set to true if models are cached
-        cache_dir = "model_cache"
         self.tokenizer = AutoTokenizer.from_pretrained(
             MODEL_PATH,
-            cache_dir=cache_dir,
             trust_remote_code=True,
             # padding_side="left"
         )
@@ -30,7 +28,6 @@ class Predictor(BasePredictor):
                 MODEL_PATH,
                 torch_dtype=TORCH_TYPE,
                 trust_remote_code=True,
-                cache_dir=cache_dir,
                 local_files_only=local_files_only
             ).eval().to(DEVICE)
         )
@@ -55,10 +52,10 @@ class Predictor(BasePredictor):
             template_version=strategy
         )
         inputs = {
-            'input_ids': inputs['input_ids'].unsqueeze(0).to(DEVICE),
-            'token_type_ids': inputs['token_type_ids'].unsqueeze(0).to(DEVICE),
-            'attention_mask': inputs['attention_mask'].unsqueeze(0).to(DEVICE),
-            'images': [[inputs['images'][0].to(DEVICE).to(TORCH_TYPE)]],
+            'input_ids': inputs['input_ids'].unsqueeze(0).to('cuda'),
+            'token_type_ids': inputs['token_type_ids'].unsqueeze(0).to('cuda'),
+            'attention_mask': inputs['attention_mask'].unsqueeze(0).to('cuda'),
+            'images': [[inputs['images'][0].to('cuda').to(TORCH_TYPE)]],
         }
         gen_kwargs = {
             "max_new_tokens": 2048,
@@ -72,13 +69,17 @@ class Predictor(BasePredictor):
             outputs = self.model.generate(**inputs, **gen_kwargs)
             outputs = outputs[:, inputs['input_ids'].shape[1]:]
             response = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
-        return response
+            return response
 
     @staticmethod
-    def load_video(video: Path, strategy='chat') -> Any:
+    def load_video(video_data: Path, strategy='chat') -> Any:
         bridge.set_bridge('torch')
-        with open(video, 'rb') as f:
+
+        video_data.open('rb').read()
+        # Open the video file and read its content
+        with open(str(video_data), 'rb') as f:
             mp4_stream = f.read()
+
         num_frames = 24
         decord_vr = VideoReader(io.BytesIO(mp4_stream), ctx=cpu(0))
         frame_id_list = None
@@ -87,7 +88,8 @@ class Predictor(BasePredictor):
             clip_end_sec = 60
             clip_start_sec = 0
             start_frame = int(clip_start_sec * decord_vr.get_avg_fps())
-            end_frame = min(total_frames, int(clip_end_sec * decord_vr.get_avg_fps())) if clip_end_sec is not None else total_frames
+            end_frame = min(total_frames,
+                            int(clip_end_sec * decord_vr.get_avg_fps())) if clip_end_sec is not None else total_frames
             frame_id_list = np.linspace(start_frame, end_frame - 1, num_frames, dtype=int)
         elif strategy == 'chat':
             timestamps = decord_vr.get_frame_timestamp(np.arange(total_frames))
